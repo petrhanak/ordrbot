5a4f972d15ed5adbb5d323d9232ba7f1096198e1
Petr Hanak <petr.hanak@strv.com>
2017-01-30 08:09:47 +0100

    show payload

--- app.js
+++ app.js
@@ -367,7 +367,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendTextMessage(senderID, `Postback called ${payload}`);
 }
 
 /*
